c76bbe053d478b2f53923c2ec6d652fd84412a17
Caian-Salvatierra <caian_salvatierra97@outlook.com>
2023-09-27 13:45:13 -0300

    Reviões do projeto

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,20 @@
- import Logo from "../../assets/portfolio.png"
- import styele from "./style.module.css"
- 
- export const Header = () => {
-    
-    return (
-         <header >
-            <div className={styele.container__header}>
-                
-            <img src={Logo} alt="logo" />
-            
-                <ul className={styele.container__item} >
-                    <li>Sobre</li>
-                    <li>Stack</li>
-                    <li>Projetos</li>
-                </ul>
-            
-                <button className="btn">Contato</button>
-            </div>
-         </header>
+import Logo from "../../assets/portfolio.png";
+import styele from "./style.module.css";
 
-    )
-}
+export const Header = () => {
+  return (
+    <header>
+      <div className={styele.container__header}>
+        <img src={Logo} alt="logo" />
+
+        <ul className={styele.container__item}>
+          <li>Sobre</li>
+          <li>Stack</li>
+          <li>Projetos</li>
+        </ul>
+
+        <button className="btn">Contato</button>
+      </div>
+    </header>
+  );
+};
